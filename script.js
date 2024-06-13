9e58bf84dff8344988dab60be99ef5c961cd6e1a
Nikil Kumar <nksiwan459@gmail.com>
2024-06-13 23:09:27 +0530

    right
    right

--- script.js
+++ script.js
@@ -10,21 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 let lastScrollTop = 0;
-document.getElementById('toggleDarkMode').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-
-    // Save the user's preference in localStorage
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-});
-
-// Check the user's preference on page load
-window.onload = function() {
-    const theme = localStorage.getItem('theme');
-    if (theme === 'dark') {
-        document.body.classList.add('dark-mode');
-    }
-};
