0d80062468f7ce9c2400d3870f030bfcfc6361ba
Nikil Kumar <nksiwan459@gmail.com>
2024-06-13 23:00:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        
+        burger.classList.toggle('toggle');
+    });
+});
+let lastScrollTop = 0;
+document.getElementById('toggleDarkMode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+
+    // Save the user's preference in localStorage
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+// Check the user's preference on page load
+window.onload = function() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+};
